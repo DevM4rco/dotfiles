8347a044410da5a1b39b5cf439b181718957f7cd
DevM4rco <devm4rco@gmail.com>
2025-11-08 18:15:26 -0300

    feat: Atualização do arquivo de testes e keymaps

--- .config/nvim/lua/marco/plugins/neotest.lua
+++ .config/nvim/lua/marco/plugins/neotest.lua
@@ -8,17 +8,24 @@ return {
 		'marilari88/neotest-vitest',
 		'nvim-neotest/neotest-jest',
 	},
-	opts = {
-		adapters = {
-			['neotest-vitest'] = {},
-			['neotest-jest'] = {},
-		},
-	},
+	config = function()
+		require('neotest').setup {
+			adapters = {
+				require 'neotest-vitest',
+				require 'neotest-jest',
+			},
+		}
+	end,
 	keys = {
 		{
 			'<leader>tr',
 			'<cmd>Neotest run<CR>',
 			desc = 'Run test',
+		},
+		{
+			'<leader>ti',
+			'<cmd>Neotest output<CR>',
+			desc = 'See outputs',
 		},
 		{
 			'<leader>ts',
