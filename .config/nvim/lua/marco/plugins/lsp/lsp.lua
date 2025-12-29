3186908e9c457fec5291ceafb471425ead1c69b8
DevM4rco <devm4rco@gmail.com>
2025-12-28 22:20:12 -0300

    Revert "feat: Configuração extra para vue"
    This reverts commit 2798f1cf45fa4881949f978ee8855ca58476fa97.

--- .config/nvim/lua/marco/plugins/lsp/lsp.lua
+++ .config/nvim/lua/marco/plugins/lsp/lsp.lua
@@ -54,17 +54,6 @@ return {
 			end,
 		})
 
-		vim.lsp.config('ts_ls', {
-			capabilities = capabilities,
-			filetypes = {
-				'typescript',
-				'javascript',
-				'javascriptreact',
-				'typescriptreact',
-				'vue',
-			},
-		})
-
 		vim.lsp.config('lua_ls', {
 			capabilities = capabilities,
 			settings = {
